d4389bae7480d30c71d2306c8fa50fe12223d84e
张波 <zhangbo@zhangbodeMacBook-Pro.local>
2014-10-27 15:14:59 +0800

    create the application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DataVisualization::Application.config.secret_token = 'fd14a255bba55643d58d1b0302436285e28b82fdfe5aa72a3d8376604ceb1908fcd12d05a2e14ec2e4c157a93fe8ef4ee4a2772bab9d0354e9b274a1d9de7eae'
